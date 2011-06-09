12df440986ef627edbdef45bf827646510b2cf55
Scott M Parrish <anithri@gmail.com>
2011-06-09 13:08:12 -0600

    changed default options keys to use strings as symbols were not being passed reliably.\n formatter respecting color now

--- lib/saw.rb
+++ lib/saw.rb
@@ -6,7 +6,7 @@ module Saw
     LOG_LEVELS = [:Footprint, :Debug, :Info, :Summary, :Warn, :Error, :Fatal]
     DEFAULT_SCREEN_LEVEL = :Info
     DEFAULT_FILE_LEVEL = :Summary
-    DEFAULT_OPTS = {:trace => false, :console_level => DEFAULT_SCREEN_LEVEL, :file_level => DEFAULT_FILE_LEVEL }
+    DEFAULT_OPTS = {'color' => true,'trace' => false, 'console_level' => DEFAULT_SCREEN_LEVEL, 'file_level' => DEFAULT_FILE_LEVEL}
     LOG_COLORS = [:grey, :cyan, :light_blue, :white, :yellow, :red, {:color => :red, :background => :white}]
 
     include Log4r
@@ -24,38 +24,39 @@ module Saw
 
     def self.setup_logger(in_opts = {})
       opts = DEFAULT_OPTS.merge(in_opts)
-      opts[:console_level_num] = find_level(opts[:console_level])
-      opts[:file_level_num] = find_level(opts[:file_level])
+      opts['console_level_num'] = find_level(opts['console_level'])
+      opts[:'file_level_num'] = find_level(opts['file_level'])
 
       Configurator.custom_levels(*LOG_LEVELS)
       Log4r::Logger.global.level = ALL
       @@logger = Log4r::Logger.new('logger')
-      @@logger.trace = opts[:trace]
+      @@logger.trace = opts['trace']
       @@logger.info(opts)
-      @@logger.add StdoutOutputter.new('stdout', :formatter=>SawFormatter, :level => opts[:console_level_num])
-      if opts[:filename] 
-        @@logger.add FileOutputter.new('logfile', :filename => opts[:filename], :level => opts[:file_level_num])
-
+      @@logger.add StdoutOutputter.new('stdout', :formatter=>SawFormatter, :level => opts['console_level_num'])
+      @@logger.outputters[0].formatter.color = opts['color']
+      if opts['filename']
+        @@logger.add FileOutputter.new('logfile', :filename => opts['filename'], :level => opts['file_level_num'])
       end
       @@logger
     end
 
     class SawFormatter <  Log4r::Formatter
- 
+
+      attr_accessor :color
       @@basicformat = "%*s %s"
 
       def initialize(hash={})
         @depth = (hash[:depth] or hash['depth'] or 7).to_i
-        @color = hash[:color] or hash['color'] or true
+        self.color = hash[:color] or hash['color'] or true
       end
 
       def format(event)
-        color = Saw::Logger::LOG_COLORS[event.level - 1]
+        event_color = Saw::Logger::LOG_COLORS[event.level - 1]
         buff = sprintf(@@basicformat, Log4r::MaxLevelLength, Log4r::LNAMES[event.level],
                event.name)
         buff << (event.tracer.nil? ? "" : "(#{event.tracer[0]})") + ": "
         buff << format_object(event.data) + "\n"
-        buff.colorize(color)
+        self.color ? buff.colorize(event_color) : buff
       end
 
       # Formats data according to its class:
